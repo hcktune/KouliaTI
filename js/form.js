00d2dac5fbac1477cc535bab8f5ce5d6ffd42c3c
aymanemontassir <aymane-hana@hotmail.fr>
2024-01-08 01:02:59 +0100

    wa akhiiiiran

--- js/form.js
+++ js/form.js
@@ -1,12 +1,12 @@
- const inputs = document.querySelectorAll(".input");
+const inputs = document.querySelectorAll(".input");
 
 
-function addcl(){
+function ajouteFocus(){
 	let parent = this.parentNode.parentNode;
 	parent.classList.add("focus");
 }
 
-function remcl(){
+function removeFocus(){
 	let parent = this.parentNode.parentNode;
 	if(this.value == ""){
 		parent.classList.remove("focus");
@@ -15,6 +15,6 @@ function remcl(){
 
 
 inputs.forEach(input => {
-	input.addEventListener("focus", addcl);
-	input.addEventListener("blur", remcl);
+	input.addEventListener("focus", ajouteFocus); 
+	input.addEventListener("blur" , removeFocus);
 });
